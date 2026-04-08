823efa4d0be6bef38f61f5e877278110a3d5973f
Kartik Choudhary <0011kartikchoudhary@gmail.com>
2026-04-08 23:17:26 +0530

    Add files via upload

--- server/app.py
+++ server/app.py
@@ -11,3 +11,10 @@ async def reset():
 @app.post("/step")
 async def step(action: dict = Body(...)):
     return env.step(action)
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
